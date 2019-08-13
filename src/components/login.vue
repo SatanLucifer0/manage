7497b635648284a72854e91b0c0a882161aedb5c
SatanLucifer0 <284679011@qq.com>
2019-08-13 20:12:56 +0800

    初始化和登录界面搭建

---
+++ src/components/login.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="login">
+    <div class="login-from">
+      <h2>登录界面</h2>
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+        label-position="top"
+      >
+        <el-form-item label="账号" prop="userName">
+          <el-input type="text" v-model="ruleForm.userName" autocomplete="off" ></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')" class="btnStyle">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        userName: "",
+        password: ""
+      },
+      rules: {
+        userName: [
+          { required: true, message: "请输入账号", trigger: "blur" },
+          { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "change" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "change" }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.login {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.login-from {
+
+  width: 460px;
+  height: 320px;
+  border-radius: 5px;
+  background-color: #fff;
+  padding: 40px;
+  h2{
+    margin-bottom: 10px;
+  }
+  .btnStyle{
+    width: 100%;
+    margin-top: 10px;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,47 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入全局基础样式
+import './assets/css/base.less'
+
+// 导入饿了么UI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
+
+
+
+// ------------------------------------------------------------
+// 导入组件
+import login from './components/login.vue'
+import index from './components/index.vue'
+
+
+
+
+// 导入路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+
+
+// 注册路由,(设置路由规则)
+const routes = [
+  { path: '/login', component: login },
+  { path: '/index', component: index }
+]
+
+
+// 创建路由
+const router = new VueRouter({
+  routes
+})
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 挂载路由
+  router
 }).$mount('#app')
